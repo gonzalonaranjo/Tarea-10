6d996963d3a5ea65aaf18b5f8b6b74e23dbc746b
gonzalonaranjo <gon.naranjo91@gmail.com>
2016-05-27 12:52:05 +0200

    Añadido el test 2

--- test/test.java
+++ test/test.java
@@ -1,5 +1,8 @@
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
+import junio2013.Anuncio;
+import junio2013.IBaseDeDatosDeAnunciantes;
+import junio2013.IBaseDeDatosDePagos;
 import junio2013.TablonDeAnuncios;
 
 import org.junit.Before;
@@ -9,7 +12,7 @@ import org.junit.Test;
 public class test {
 
 	private TablonDeAnuncios tab;
-	
+
 	@Before
 	public void setUp(){
 		tab = new TablonDeAnuncios();
@@ -19,5 +22,17 @@ public class test {
 	public void crearUnAnuncioHayUnAnuncio(){
 		assertEquals(1, tab.anunciosPublicados());
 	}
-
+	
+	@Test
+	public void publicarUnAnuncioYVerQueSeIncrementaElNumero(){
+		Anuncio anuncio = new Anuncio("2 anuncio", "segundo anuncio", "LA EMPRESA");
+		//creamos objeto Mock
+		IBaseDeDatosDeAnunciantes bdAnunciantes = mock(IBaseDeDatosDeAnunciantes.class);
+		IBaseDeDatosDePagos bdPagos = mock(IBaseDeDatosDePagos.class);
+		
+		tab.publicarAnuncio(anuncio, bdAnunciantes, bdPagos);
+		
+		assertEquals(2, tab.anunciosPublicados());
+		
+	}
 }
